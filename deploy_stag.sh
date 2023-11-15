a6f8ecf67e808323fbb7084e551c31440e843832
Sayem Hossain <mail@sayem.dev>
2023-11-15 23:52:00 +0600

    Updated dev script

--- deploy_stag.sh
+++ deploy_stag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo fuser -k 9090/tcp
-sudo nohup java -Xmx512m -Dserver.port=9090 -jar app-stag/app-stag.jar >> app-stag/app-stag.log >&1 &
+sudo nohup java -Xmx512m -Dserver.port=9090 -jar app-stag/app-stag.jar >> app-stag/app-stag.log 2>&1 &
