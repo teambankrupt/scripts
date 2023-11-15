a98dd0e28299ae6035c544cf5c270a8adfee7675
Sayem Hossain <mail@sayem.dev>
2023-11-15 23:44:48 +0600

    Updated dev script

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo fuser -k 8080/tcp
-sudo nohup java -Xmx512m -Dserver.port=8080 -jar app-dev/app-dev.jar >> app-dev/app-dev.log >&1 &
+sudo nohup java -Xmx512m -Dserver.port=8080 -jar app-dev/app-dev.jar >> app-dev/app-dev.log 2>&1 &
