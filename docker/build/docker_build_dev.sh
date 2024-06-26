3674ba6bc4ca021c374f0e2413de901350bfcba9
Sayem Hossain <mail@sayem.dev>
2024-06-27 00:29:58 +0600

    updated deployment script

--- docker/build/docker_build_dev.sh
+++ docker/build/docker_build_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn -f pom.xml clean package -DskipTests -Denv=dev --batch-mode --errors --fail-at-end --show-version
+mvn -f pom.xml clean package -DskipTests -Denv=dev -Dmaven.repo.local=.m2/repository --batch-mode --errors --fail-at-end --show-version
 docker build -t sayemoid/cognito_dev .
