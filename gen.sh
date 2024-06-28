b11a1c6da977286a4bef9d24c0366e127ad00945
Sayem Hossain <mail@sayem.dev>
2024-06-28 11:13:57 +0600

    updated deployment script

--- gen.sh
+++ gen.sh
@@ -13,4 +13,6 @@ elif [ "$1" = "module" ]; then
 elif [ "$1" = "migration" ]; then
   echo "-- $(date "+%b %d, %Y")" >"app/src/main/resources/db/migration/V$(date +%s)__$2.sql"
   echo "Migration Created!"
+else
+  echo "Must specify type of generated asset. i.e. crud | module | migration"
 fi
