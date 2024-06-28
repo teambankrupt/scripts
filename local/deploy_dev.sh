cd691bb422d169c17ac567b6e304209e7ee7a085
Sayem Hossain <mail@sayem.dev>
2024-06-28 14:35:02 +0600

    updated deployment script

--- local/deploy_dev.sh
+++ local/deploy_dev.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 DATA_DIR="/var/cognito/dev"
 mkdir -p $DATA_DIR
+docker compose -f /var/cognito/deployment/app_dev.yml down
+
+docker rmi -f sayemoid/cognito_dev
 docker compose -f /var/cognito/deployment/app_dev.yml up
