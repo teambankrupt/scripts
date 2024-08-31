8671bb75a7583faeb254790c49f6229e7b148126
Sayem Hossain <mail@sayem.dev>
2024-09-01 00:40:08 +0600

    maven build phase to install from package

--- docker/build/docker_build_dev.sh
+++ docker/build/docker_build_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Building jar file.."
-mvn -f pom.xml clean package -DskipTests -Denv=dev -Dmaven.repo.local=.m2/repository --batch-mode --errors --fail-at-end --show-version
+mvn -f pom.xml clean install -DskipTests -Denv=dev -Dmaven.repo.local=.m2/repository --batch-mode --errors --fail-at-end --show-version
 echo "Building docker image.."
 docker build -t sayemoid/cognito_dev .
